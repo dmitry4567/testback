0d9b6c044a9bb2baddb6e17143ba2ccf112f3d21
dmitry4567 <dimon2003pp@gmail.com>
2024-02-02 14:35:22 +0300

    init + config

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = parseInt(process.env.PORT);
+  const server = process.env.SERVER;
+  await app.listen(port, server);
+}
+bootstrap();
